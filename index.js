ccd12036c38f19fe735a463edec96560048f3303
bfg9kappa <bfg9kappa@gmail.com>
2022-10-14 08:54:18 +0200

    projecte base

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 5556;
+
+app.get('/', function(req, res) {
+  
+  res.send('Hello express!');
+});
+const server = app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
