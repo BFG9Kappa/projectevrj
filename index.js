2df904c7309b8f54cc7449aa4b7dfc9a4a139d0f
bfg9kappa <bfg9kappa@gmail.com>
2022-10-14 08:57:30 +0200

    prova

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 5556;
 
 app.get('/', function(req, res) {
   
-  res.send('Hello express!');
+  res.send('Prova!');
 });
 const server = app.listen(port, () => {
   console.log(`Server running on port ${port}`);
