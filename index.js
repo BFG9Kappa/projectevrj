e35ecb6cc451f2a43c9f25da4b1b49c81155fa13
josep <bfg9kappa@gmail.com>
2022-10-15 18:18:57 +0200

    update

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 const port = 5556;
 
 app.get('/', function(req, res) {
-  
   res.send('Prova!');
 });
+
 const server = app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
