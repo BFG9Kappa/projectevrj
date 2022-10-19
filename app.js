892b3c36b01c16f1981f3ec170ae286952832311
josep <bfg9kappa@gmail.com>
2022-10-19 15:41:44 +0200

    update

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const port = 5556;
 
+// Conexio a base de dades
+const mongoose = require('mongoose');
+
+
 app.get('/', function(req, res) {
   res.send('Prova!');
 });
