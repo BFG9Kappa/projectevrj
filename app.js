d27b846516d23de97d5b75c2c30b1c506af07c86
bfg9kappa <bfg9kappa@gmail.com>
2022-10-20 10:28:03 +0200

    update

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require("express");
+const path = require('path');
 const app = express();
-const port = 5556;
+const port = process.env.PORT || 5556;
 
-// Conexio a base de dades
+// Conexio a base de dades - Implementar
 const mongoose = require('mongoose');
+
+// Ruta de les vistes
+app.set('views',path.join(__dirname,'views'));
+// Selecciona motor de plantilles EJS
+app.set('view engine', 'ejs');
 
 
 app.get('/', function(req, res) {
-  res.send('Prova!');
+  //res.send('Prova!');
+  res.render('pages/index'); // Retorna la vista 
 });
+
 
 const server = app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
