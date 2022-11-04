e5a8ddbd2fc4b5d81a1567d29679d0214b2b677e
bfg9kappa <bfg9kappa@gmail.com>
2022-11-04 09:47:12 +0100

    horari view i schema

--- routes/horariRouter.js
+++ routes/horariRouter.js
@@ -1,8 +1,20 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/", function (req, res) {  
-    res.render('login');
-});
+const horari_controller = require("../controllers/horariController");
+
+
+router.get("/", horari_controller.list);
+
+/*
+router.get("/create", horari_controller.create_get);
+router.post("/create", horari_controller.create_post);
+
+router.get("/delete/:id", horari_controller.delete_get);
+router.post("/delete/:id", horari_controller.delete_post);
+
+router.get("/update/:id", horari_controller.update_get);
+router.post("/update/:id", horari_controller.update_post);
+*/
 
 module.exports = router;
