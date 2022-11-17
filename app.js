c28e3bf9897a8373058fc287d091c8e4d8977f86
Robert <robertrlm16@gmail.com>
2022-11-17 09:00:07 +0100

    app.js

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ var indexRouter = require('./routes/indexRouter');
 var loginRouter = require('./routes/loginRouter');
 var usuarisRouter = require('./routes/usuarisRouter');
 var horariRouter = require('./routes/horariRouter');
-//var baixesmediquesRouter = require('./routes/baixesmediquesRouter');
+var baixesmediquesRouter = require('./routes/baixesmediquesRouter');
+var absnoprevistesRouter = require('./routes/absnoprevistesRouter');
 
 
 dotenv.config();
@@ -48,6 +49,7 @@ app.use('/home', indexRouter);
 app.use('/login', loginRouter);
 app.use('/usuaris', usuarisRouter);
 app.use('/horaris', horariRouter);
-//app.use('/baixesmediques', baixesmediquesRouter);
+app.use('/baixesmediques', baixesmediquesRouter);
+app.use('/absnoprevistes', absnoprevistesRouter);
 
 module.exports = app;
