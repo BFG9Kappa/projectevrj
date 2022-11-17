5a00d54d66a3abf1d96697ae142f4d93c3519649
Robert <robertrlm16@gmail.com>
2022-11-17 08:55:17 +0100

    implementasion robertiana

---
+++ models/absnoprevista.js
@@ -0,0 +1,16 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var AbsNoPrevistaSchema = new Schema({
+        data_absnoprevista: { type: Date, required: true},
+        horari_profe: { type: Date, required: true},
+        motiu_abs:{ type: String},
+
+      });
+
+
+// Export model.
+module.exports = mongoose.model("AbsNoPrevista", AbsNoPrevistaSchema);
+
+
