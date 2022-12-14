a8ec729d23d66bd8a32d72cfa3ed341576056734
bfg9kappa <bfg9kappa@gmail.com>
2022-12-14 10:46:34 +0100

    horaris

--- controllers/horariController.js
+++ controllers/horariController.js
@@ -1,8 +1,30 @@
 var Horari = require("../models/horari");
 
+//const { body, validationResult } = require("express-validator"); // Validacions
+
 class HorariController {
 
-
+  /* //Validacions
+  static rules = [
+    // Validate and sanitize fields.
+    body("name")
+      .trim()
+      .isLength({ min: 1})
+      .withMessage('Name must not be empty.')
+      .isLength({ max: 20})
+      .withMessage('Name is too long.')
+      .escape()
+      .custom(async function(value, {req}) {
+          const genre = await Genre.findOne({name:value});
+          if (genre) {
+            if(req.params.id!==genre.id ) {
+              throw new Error('This gender name already exists.');
+            }
+          }
+          return true;
+      })
+  ];
+  */
 
   static list(req, res, next) {
     Horari.find()
@@ -10,9 +32,24 @@ class HorariController {
         if (err) {
           return next(err);
         }
-        //res.send('prova horari');
         res.render('horaris/list', { list: list_horari })
       });
+  }
+
+  static create_get(req, res, next) {
+    res.render('horaris/new');
+  }
+
+  static create_post(req, res) {
+    // console.log(req.body)
+    Horari.create(req.body, function (error, newHorari)  {
+        if(error){
+            //console.log(error)
+            res.render('horaris/new',{error:error.message})
+        }else{             
+            res.redirect('/horaris')
+        }
+    })    
   }
 
 }
--- routes/horariRouter.js
+++ routes/horariRouter.js
@@ -5,10 +5,10 @@ const horari_controller = require("../controllers/horariController");
 
 router.get("/", horari_controller.list);
 
-/*
 router.get("/create", horari_controller.create_get);
 router.post("/create", horari_controller.create_post);
 
+/*
 router.get("/delete/:id", horari_controller.delete_get);
 router.post("/delete/:id", horari_controller.delete_post);
 
