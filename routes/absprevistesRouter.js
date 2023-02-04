d988bf9ff09ecf96157190e7265a73a5b692647d
josep <bfg9kappa@gmail.com>
2023-02-04 14:50:03 +0100

    rutes

--- routes/absprevistesRouter.js
+++ routes/absprevistesRouter.js
@@ -6,10 +6,9 @@ const absprevista_controller = require("../controllers/absprevistesController");
 router.get("/", absprevista_controller.list);
 router.get("/create", absprevista_controller.create_get);
 router.post("/create", absprevista_controller.create_post);
-/*
 router.get("/update/:id", absprevista_controller.update_get);
 router.post("/update/:id", absprevista_controller.update_post);
 router.get("/delete/:id", absprevista_controller.delete_get);
 router.post("/delete/:id", absprevista_controller.delete_post);
-*/
+
 module.exports = router;
