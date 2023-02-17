463986494c831251c998d9c2274491dd58bc8860
bfg9kappa <bfg9kappa@gmail.com>
2023-02-17 09:28:59 +0100

    update seeder i baixa schema

--- dataSeeders/seederBaixesmediques.js
+++ dataSeeders/seederBaixesmediques.js
@@ -7,12 +7,7 @@ var Baixa = require("../models/baixamedica");
 // Carregar dades d'un CSV
 const csv = require('csvtojson')
 
-// 
 var mongoDB = process.env.MONGODB_URI;
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
-//mongoose.Promise = global.Promise;
-//var db = mongoose.connection;
-
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(
         function () {
@@ -23,20 +18,6 @@ mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
         }
     )
     .catch("when the error happened")
-
-/*
-db.on('connected', function () {
-  console.log('Mongoose connection open'); 
-  
-  seeder().then( function() {
-    mongoose.connection.close();
-  });
-
-});
-
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-*/
 
 async function seeder() {
     // Esborrar contingut col·leccions
