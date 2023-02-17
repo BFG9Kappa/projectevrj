2b8c597d555df18592f53d99d59fbed0b48a2eda
josep <bfg9kappa@gmail.com>
2023-02-17 23:19:45 +0100

    refactor

--- controllers/absprevistesController.js
+++ controllers/absprevistesController.js
@@ -1,93 +1,88 @@
 var AbsenciaPrevista = require("../models/absprevista");
 
 class absprevistaController {
-  
-    static list(req, res, next) {
-      AbsenciaPrevista.find()
-          .exec(function (err, list_absprevista) {
-            if (err) {
-              return next(err);
-            }
-              res.render('absprevistes/list',{ list: list_absprevista })
-        });
-    }
-  
-  static create_get(req, res, next) {
-      
-        var AbsenciaPrevista = {
-          "data_absprevista" : "",
-          "motiu_abs" : ""
-        }
-        res.render('absprevistes/new',{AbsenciaPrevista:AbsenciaPrevista});
-    }
-  
+	static list(req, res, next) {
+		AbsenciaPrevista.find().exec(function (err, list_absprevista) {
+			if (err) {
+				return next(err);
+			}
+			res.render("absprevistes/list", { list: list_absprevista });
+		});
+	}
 
-  static create_post(req, res, next) {
+	static create_get(req, res, next) {
+		var AbsenciaPrevista = {
+			data_absprevista: "",
+			motiu_abs: "",
+		};
+		res.render("absprevistes/new", { AbsenciaPrevista: AbsenciaPrevista });
+	}
 
-    AbsenciaPrevista.create(req.body, (error, newRecord) => {
-      if (error) {
-        res.render('absprevistes/new', { error: 'error' })
-      } else {
+	static create_post(req, res, next) {
+		AbsenciaPrevista.create(req.body, (error, newRecord) => {
+			if (error) {
+				res.render("absprevistes/new", { error: "error" });
+			} else {
+				res.redirect("/absprevistes");
+			}
+		});
+	}
 
-        res.redirect('/absprevistes')
-      }
-    })
-  }
-   
-    static update_get(req, res, next) {
-      AbsenciaPrevista.findById(req.params.id, function (err, absenciaprevista) {
-          if (err) {
-            return next(err);
-          }
-          if (absenciaprevista == null) {
-            // No results.
-            var err = new Error("Absencia prevista not found");
-            err.status = 404;
-            return next(err);
-          }
-          // Success.
-          res.render("absprevistes/update", { absenciaprevista: absenciaprevista });
-          
-      });
-        
-    }
-  
-    static update_post(req, res, next) {
-        var absenciaprevista = new AbsenciaPrevista({
-          data_absprevista: req.body.data_absprevista,
-          motiu_abs: req.body.motiu_abs,
-          _id: req.params.id,
-        });
+	static update_get(req, res, next) {
+		AbsenciaPrevista.findById(req.params.id, function (err, absenciaprevista) {
+			if (err) {
+				return next(err);
+			}
+			if (absenciaprevista == null) {
+				// No results.
+				var err = new Error("Absència prevista no trobada");
+				err.status = 404;
+				return next(err);
+			}
+			// Success.
+			res.render("absprevistes/update", { absenciaprevista: absenciaprevista });
+		});
+	}
 
-        AbsenciaPrevista.findByIdAndUpdate(
-          req.params.id,
-          absenciaprevista,
-          { runValidators: true },
-          function (err, theAbsenciaPrevista) {
-            if (err) {
-              res.render("absprevistes/update", { absenciaprevista: absenciaprevista, error: err.message });
-            }
-            res.render("absprevistes/update", { absenciaprevista: absenciaprevista, message: 'Absencia Prevista Actualitzada'});
-          
-          }
-        );
-    }
- 
-    static async delete_get(req, res, next) {
-        
-        res.render('absprevistes/delete',{id: req.params.id})
-    }
-  
-    static async delete_post(req, res, next) {
-      AbsenciaPrevista.findByIdAndRemove(req.params.id, (error)=> {
-        if(error){
-          res.redirect('/absprevistes')
-        }else{
-          res.redirect('/absprevistes')
-        }
-      }) 
-    }
-  
+	static update_post(req, res, next) {
+		var absenciaprevista = new AbsenciaPrevista({
+			data_absprevista: req.body.data_absprevista,
+			motiu_abs: req.body.motiu_abs,
+			_id: req.params.id,
+		});
+
+		AbsenciaPrevista.findByIdAndUpdate(
+			req.params.id,
+			absenciaprevista,
+			{ runValidators: true },
+			function (err, theAbsenciaPrevista) {
+				if (err) {
+					res.render("absprevistes/update", {
+						absenciaprevista: absenciaprevista,
+						error: err.message,
+					});
+				}
+				res.render("absprevistes/update", {
+					absenciaprevista: absenciaprevista,
+					message: "Absència prevista actualitzada",
+				});
+			}
+		);
+	}
+
+	static async delete_get(req, res, next) {
+		res.render("absprevistes/delete", { id: req.params.id });
+	}
+
+	static async delete_post(req, res, next) {
+		AbsenciaPrevista.findByIdAndRemove(req.params.id, (error) => {
+			if (error) {
+				res.redirect("/absprevistes");
+			} else {
+				res.redirect("/absprevistes");
+			}
+		});
+	}
 }
 
 module.exports = absprevistaController;
--- controllers/baixesmediquesController.js
+++ controllers/baixesmediquesController.js
@@ -1,81 +1,85 @@
 var BaixaMedica = require("../models/baixamedica");
 
 class baixesmediquesController {
+	static async list(req, res, next) {
+		try {
+			var list_baixesmediques = await BaixaMedica.find();
+			res.render("baixesmediques/list", { list: list_baixesmediques });
+		} catch (e) {
+			res.send("Error!");
+		}
+	}
 
-  static async list(req, res, next) {
-    try {
-      var list_baixesmediques = await BaixaMedica.find();
-      res.render('baixesmediques/list', { list: list_baixesmediques })
-    } catch (e) {
-      res.send('Error!');
-    }
-  }
+	static create_get(req, res, next) {
+		res.render("baixesmediques/new");
+	}
 
-  static create_get(req, res, next) {
-    res.render('baixesmediques/new');
-  }
+	static create_post(req, res) {
+		// console.log(req.body)
+		BaixaMedica.create(req.body, function (error, newBaixamedica) {
+			if (error) {
+				//console.log(error)
+				res.render("baixesmediques/new", { error: error.message });
+			} else {
+				res.redirect("/baixesmediques");
+			}
+		});
+	}
 
-  static create_post(req, res) {
-    // console.log(req.body)
-    BaixaMedica.create(req.body, function (error, newBaixamedica) {
-      if (error) {
-        //console.log(error)
-        res.render('baixesmediques/new', { error: error.message })
-      } else {
-        res.redirect('/baixesmediques')
-      }
-    })
-  }
+	static update_get(req, res, next) {
+		BaixaMedica.findById(req.params.id, function (err, baixamedica) {
+			if (err) {
+				return next(err);
+			}
+			if (baixamedica == null) {
+				// No results.
+				var err = new Error("Baixa medica no trobada");
+				err.status = 404;
+				return next(err);
+			}
+			// Success.
+			res.render("baixesmediques/update", { baixamedica: baixamedica });
+		});
+	}
 
-  static update_get(req, res, next) {
-    BaixaMedica.findById(req.params.id, function (err, baixamedica) {
-      if (err) {
-        return next(err);
-      }
-      if (baixamedica == null) {
-        // No results.
-        var err = new Error("BaixaMedica not found");
-        err.status = 404;
-        return next(err);
-      }
-      // Success.
-      res.render("baixesmediques/update", { baixamedica: baixamedica });
-    });
-  }
-  
-  static update_post(req, res, next) {
-      var baixamedica = new BaixaMedica({
-        name: req.body.name,
-        _id: req.params.id,
-      });    
-    
-      BaixaMedica.findByIdAndUpdate(
-        req.params.id,
-        baixamedica,
-        {},
-        function (err, baixamedicaFound) {
-          if (err) {
-            res.render("baixesmediques/update", { baixamedica: baixamedica, error: err.message });
-          }
-          res.render("baixesmediques/update", { baixamedica: baixamedica, message: 'Publisher Updated'});
-        }
-      );
-  }
-  
-  static async delete_get(req, res, next) {
-    res.render('baixesmediques/delete',{id: req.params.id})
-  }
+	static update_post(req, res, next) {
+		var baixamedica = new BaixaMedica({
+			name: req.body.name,
+			_id: req.params.id,
+		});
 
-  static async delete_post(req, res, next) {
-    BaixaMedica.findByIdAndRemove(req.params.id, (error)=> {
-      if(error){
-        res.redirect('/baixesmediques')
-      }else{
-        res.redirect('/baixesmediques')
-      }
-    })
-  }
+		BaixaMedica.findByIdAndUpdate(
+			req.params.id,
+			baixamedica,
+			{},
+			function (err, baixamedicaFound) {
+				if (err) {
+					res.render("baixesmediques/update", {
+						baixamedica: baixamedica,
+						error: err.message,
+					});
+				}
+				res.render("baixesmediques/update", {
+					baixamedica: baixamedica,
+					message: "Baixa medica actualitzada",
+				});
+			}
+		);
+	}
 
+	static async delete_get(req, res, next) {
+		res.render("baixesmediques/delete", { id: req.params.id });
+	}
+
+	static async delete_post(req, res, next) {
+		BaixaMedica.findByIdAndRemove(req.params.id, (error) => {
+			if (error) {
+				res.redirect("/baixesmediques");
+			} else {
+				res.redirect("/baixesmediques");
+			}
+		});
+	}
 }
 
 module.exports = baixesmediquesController;
