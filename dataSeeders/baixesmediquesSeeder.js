b7b3af9fe0cc239bfc8bdda8e0d1caba15835b45
bfg9kappa <bfg9kappa@gmail.com>
2023-02-23 10:27:18 +0100

    update seeders, arreglo

--- dataSeeders/baixesmediquesSeeder.js
+++ dataSeeders/baixesmediquesSeeder.js
@@ -1,30 +1,29 @@
-require('dotenv').config({ path: '../.env' })
-const mongoose = require('mongoose');
+require("dotenv").config({ path: "../.env" });
+const mongoose = require("mongoose");
 
 // Importar models
-var Baixa = require("../models/baixamedica");
+const Baixa = require("../models/baixamedica");
 
 // Carregar dades d'un CSV
-const csv = require('csvtojson')
-
-var mongoDB = process.env.MONGODB_URI;
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(
-        function () {
-            console.log('Mongoose connection open');
-            seeder().then(function () {
-                mongoose.connection.close();
-            });
-        }
-    )
-    .catch("when the error happened")
+const csv = require("csvtojson");
 
 async function seeder() {
-    // Esborrar contingut col·leccions
-    await Baixa.collection.drop();
-
-    // Inserir dades
-    const jsonObj = await csv()
-        .fromFile('./dataBaixes.csv')
-    baixaList = await Baixa.insertMany(jsonObj);
+	// Esborrar contingut col·leccions
+	await Baixa.collection.drop();
+	// Inserir dades
+	const jsonObj = await csv().fromFile("./dataBaixes.csv");
+	baixaList = await Baixa.insertMany(jsonObj);
 }
+
+const mongoDB = process.env.MONGODB_URI;
+mongoose
+	.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(function () {
+		console.log("Sembrant: Baixes mediques");
+		seeder().then(function () {
+			mongoose.connection.close();
+		});
+	})
+	.catch((error) => {
+		console.log("when the error happened", error);
+	});
