a60a11d44528dc62e6e895f3eccc5345ec3b180b
Josep Calvet <45820087+BFG9Kappa@users.noreply.github.com>
2023-02-23 09:04:22 +0100

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const mongoose = require("mongoose");
 var mongoDB = process.env.MONGODB_URI;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 // Paquete para las fechas
 app.locals.moment = require('moment');
