0efa67e67fbe64628900b8ab63ebbc63f5c2da20
Robert <robertrlm16@gmail.com>
2023-04-13 09:49:31 +0200

    validaciones update sortides y absprevistes

--- controllers/absprevistesController.js
+++ controllers/absprevistesController.js
@@ -1,132 +1,162 @@
 var AbsenciaPrevista = require("../models/absprevista");
+const moment = require("moment");
 const { body, validationResult } = require("express-validator");
 
 class absprevistaController {
 
-	static rules = [
-		body("data_absprevista", "La data de l'absència prevista no pot estar buida.")
-			.trim()
-			.isLength({ min: 1 })
-			.escape(),
-
-		body("motiu_abs", "El motiu de l'absència no pot estar buit.")
-			.trim()
-			.isLength({ min: 1 })
-			//.escape()
-			,
-
-			body("motiu_abs", "El motiu ha de tindre com a mínim 5 caràcters.")
+  static rules = [
+    body("data_absprevista", "La data de l'absència prevista no pot estar buida.")
       .trim()
-      .isLength({ min: 5})
-      //.escape()
-			,
-	];
-	static async list(req, res, next) {
-		try {
-			var list_absprevistes = await AbsenciaPrevista.find();
-			res.render("absprevistes/list", { list: list_absprevistes });
-		} catch (error) {
-			res.send(error);
-		}
-	}
+      .isLength({ min: 1 })
+      .escape(),
+    body("data_absprevista").custom((value, { req }) => {
+      const data_actual = moment(req.body.data_actual, "DD-MM-YYYY");
+      const data_absprevista = moment(value, "DD-MM-YYYY");
+      if (data_absprevista.isBefore(data_actual)) {
+        throw new Error(
+          "La data de l'absencia prevista ha de ser posterior a la data actual"
+        );
+      }
+      return true;
+    }),
+    body("motiu_abs")
+    .trim()
+    .custom((value, { req }) => {
+      if (!value) {
+        throw new Error("El motiu de l'absència no pot estar buit.");
+      }
+      if (value.length < 5) {
+        throw new Error("El motiu ha de tindre com a mínim 5 caràcters.");
+      }
+      return true;
+    })
+    .escape()
+  ];
+  static async list(req, res, next) {
+    try {
+      var list_absprevistes = await AbsenciaPrevista.find();
+      res.render("absprevistes/list", { list: list_absprevistes });
+    } catch (error) {
+      res.send(error);
+    }
+  }
 
-	static genpdf_get(req, res, next) {
-		res.render("absprevistes/decresp");
-	}
+  static genpdf_get(req, res, next) {
+    res.render("absprevistes/decresp");
+  }
 
-	static genpdf_post(req, res, next) {
-		res.redirect("/absprevistes");
-	}
+  static genpdf_post(req, res, next) {
+    res.redirect("/absprevistes");
+  }
 
-	static create_get(req, res, next) {
-		var AbsenciaPrevista = {
-			data_absprevista: "",
-			motiu_abs: "",
-		};
-		res.render("absprevistes/new", { AbsenciaPrevista: AbsenciaPrevista });
-	}
+  static create_get(req, res, next) {
+    var absenciaprevista = {
+      data_absprevista: "",
+      motiu_abs: "",
+    };
+    res.render("absprevistes/new", { absenciaprevista: absenciaprevista });
+  }
 
-	static create_post(req, res) {
-		const errors = validationResult(req);
-		//console.log(errors.array());
-		// Tenim errors en les dades enviades
+  static create_post(req, res) {
+    const errors = validationResult(req);
+    //console.log(errors.array());
+    // Tenim errors en les dades enviades
 
-		if (!errors.isEmpty()) {
-			var absenciaprevista = {
-				data_absprevista: req.body.data_absprevista,
-				motiu_abs: req.body.motiu_abs,
-				_id: req.params.id,
-			};
-			res.render("absprevistes/new", {
-				errors: errors.array(),
-				absenciaprevista: absenciaprevista,
-			});
-		} else {
-			AbsenciaPrevista.create(req.body, function (error, newAbsenciaPrevista) {
-				if (error) {
-					res.render("absprevistes/new", { error: error.message });
-				} else {
-					res.redirect("/absprevistes");
-				}
-			});
-		}
-	}
+    if (!errors.isEmpty()) {
+      var absenciaprevista = {
+        data_absprevista: req.body.data_absprevista,
+        motiu_abs: req.body.motiu_abs,
+        _id: req.params.id,
+      };
+      res.render("absprevistes/new", {
+        errors: errors.array(),
+        absenciaprevista: absenciaprevista,
+      });
+    } else {
+      AbsenciaPrevista.create(req.body, function (error, newAbsenciaPrevista) {
+        if (error) {
+          res.render("absprevistes/new", { error: error.message });
+        } else {
+          res.redirect("/absprevistes");
+        }
+      });
+    }
+  }
 
-	static update_get(req, res, next) {
-		AbsenciaPrevista.findById(req.params.id, function (err, absenciaprevista) {
-			if (err) {
-				return next(err);
-			}
-			if (absenciaprevista == null) {
-				// No results.
-				var err = new Error("Absència prevista no trobada");
-				err.status = 404;
-				return next(err);
-			}
-			// Success.
-			res.render("absprevistes/update", { absenciaprevista: absenciaprevista });
-		});
-	}
+  static update_get(req, res, next) {
+    AbsenciaPrevista.findById(req.params.id, function (err, absenciaprevista) {
+      if (err) {
+        return next(err);
+      }
+      if (absenciaprevista == null) {
+        // No results.
+        var err = new Error("Absència prevista no trobada");
+        err.status = 404;
+        return next(err);
+      }
+      // Success.
+      res.render("absprevistes/update", { absenciaprevista: absenciaprevista });
+    });
+  }
 
-	static update_post(req, res, next) {
-		var absenciaprevista = new AbsenciaPrevista({
-			data_absprevista: req.body.data_absprevista,
-			motiu_abs: req.body.motiu_abs,
-			_id: req.params.id,
-		});
+  static async update_post(req, res, next) {
+    // Obtener la fecha de salida del formulario update
+    const dataPrevista = req.body.data_absprevista;
 
-		AbsenciaPrevista.findByIdAndUpdate(
-			req.params.id,
-			absenciaprevista,
-			{ runValidators: true },
-			function (err, theAbsenciaPrevista) {
-				if (err) {
-					res.render("absprevistes/update", {
-						absenciaprevista: absenciaprevista,
-						error: err.message,
-					});
-				}
-				res.render("absprevistes/update", {
-					absenciaprevista: absenciaprevista,
-					message: "Absència prevista actualitzada",
-				});
-			}
-		);
-	}
+    // Comprobar si la data prevista existeix
+    if (!dataPrevista) {
+        // Si la fecha de salida es anterior a la fecha actual, mostrar un mensaje de error
+        return res.render("absprevistes/update", {
+            error: "La data prevista no pot estar buida",
+            absenciaprevista: req.body,
+        });
+    }
+    // Obtener la fecha de salida del formulario update
+    const motiu = req.body.motiu_abs;
 
-	static async delete_get(req, res, next) {
-		res.render("absprevistes/delete", { id: req.params.id });
-	}
+    // Comprobar si la data de sortida existeix
+    if (!motiu) {
+        // Comprobar si el motiu de l'absencia prevista existeix
+        return res.render("absprevistes/update", {
+            error: "El motiu no pot estar buit",
+            absenciaprevista: req.body,
+        });
+    }
+    var absenciaprevista = new AbsenciaPrevista({
+      data_absprevista: req.body.data_absprevista,
+      motiu_abs: req.body.motiu_abs,
+      _id: req.params.id,
+    });
 
-	static async delete_post(req, res, next) {
-		AbsenciaPrevista.findByIdAndRemove(req.params.id, (error) => {
-			if (error) {
-				res.render("absprevistes", { error: error.message });
-			} else {
-				res.redirect("/absprevistes");
-			}
-		});
-	}
+    try {
+      await AbsenciaPrevista.findByIdAndUpdate(req.params.id, {
+        data_absprevista: req.body.data_absprevista,
+        motiu_abs: req.body.motiu_abs,
+      });
+      res.redirect('/absprevistes');
+  } catch (err) {
+      res.render("absprevistes/update", {
+          error: err.message,
+          absenciaprevista: req.body,
+      });
+  }
+
+
+  }
+
+  static async delete_get(req, res, next) {
+    res.render("absprevistes/delete", { id: req.params.id });
+  }
+
+  static async delete_post(req, res, next) {
+    AbsenciaPrevista.findByIdAndRemove(req.params.id, (error) => {
+      if (error) {
+        res.render("absprevistes", { error: error.message });
+      } else {
+        res.redirect("/absprevistes");
+      }
+    });
+  }
 }
 
 module.exports = absprevistaController;
--- controllers/sortidescurricularsController.js
+++ controllers/sortidescurricularsController.js
@@ -1,137 +1,136 @@
 var SortidaCurricular = require("../models/sortidacurricular");
 const moment = require("moment");
 const { body, validationResult } = require("express-validator");
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
 class sortidacurricularController {
-	static rules = [
-		body("data_sortida", "La data de sortida no pot estar buida.")
-			.trim()
-			.isLength({ min: 1 })
-			.escape(),
-		body("data_sortida").custom((value, { req }) => {
-			const data_actual = moment(
-				req.body.data_actual,
-				"DD-MM-YYYY"
-			);
-			const data_sortida = moment(value, "DD-MM-YYYY");
-			if (data_sortida.isBefore(data_actual)) {
-				throw new Error(
-					"La data de la sortida curricular ha de ser posterior a la data actual"
-				);
-			}
-			return true;
-		}),
-	];
+  static rules = [
+    body("data_sortida", "La data de sortida no pot estar buida.")
+      .trim()
+      .isLength({ min: 1 })
+      .escape(),
+    body("data_sortida").custom((value, { req }) => {
+      const data_actual = moment(req.body.data_actual, "DD-MM-YYYY");
+      const data_sortida = moment(value, "DD-MM-YYYY");
+      if (data_sortida.isBefore(data_actual)) {
+        throw new Error(
+          "La data de la sortida curricular ha de ser posterior a la data actual"
+        );
+      }
+      return true;
+    }),
+  ];
 
-	static async list(req, res, next) {
-		try {
-			var list_sortidescurriculars = await SortidaCurricular.find();
-			res.render("sortidescurriculars/list", { list: list_sortidescurriculars });
-		} catch (e) {
-			res.send("Error!");
-		}
-	}
+  static async list(req, res, next) {
+    try {
+      var list_sortidescurriculars = await SortidaCurricular.find();
+      res.render("sortidescurriculars/list", {
+        list: list_sortidescurriculars,
+      });
+    } catch (error) {
+      res.send(error);
+    }
+  }
 
-	static create_get(req, res, next) {
-		var sortidacurricular = {
-			data_actual: "",
-			data_sortida: "",
-			email: "",
-			lloc: "",
-			ruta: "",
-			objectius: "",
-			grups: "",
-			professors: "",
-			hora_inici: "",
-			hora_arribada: "",
-			comentari: "",
-			estat:"",
-			_id: "",
-		};
-		res.render("sortidescurriculars/new", { sortidacurricular: sortidacurricular });
-	}
+  static create_get(req, res, next) {
+    var sortidacurricular = {
+      data_actual: "",
+      data_sortida: "",
+      email: "",
+      lloc: "",
+      ruta: "",
+      objectius: "",
+      grups: "",
+      professors: "",
+      hora_inici: "",
+      hora_arribada: "",
+      comentari: "",
+      estat: "",
+      _id: "",
+    };
+    res.render("sortidescurriculars/new", {
+      sortidacurricular: sortidacurricular,
+    });
+  }
 
-	static create_post(req, res) {
-		const errors = validationResult(req);
-		console.log(errors.array());
-		// Tenim errors en les dades enviades
-		if (!errors.isEmpty()) {
-			var sortidacurricular = {
-				data_sortida: req.body.data_sortida,
-				email: req.body.email,
-				lloc: req.body.lloc,
-				ruta: req.body.ruta,
-				objectius: req.body.objectius,
-				grups: req.body.grups,
-				professors: req.body.professors,
-				hora_inici: req.body.hora_inici,
-				hora_arribada: req.body.hora_arribada,
-				estat: req.body.estat,
-				_id: req.params.id,
-			};
-			res.render("sortidescurriculars/new", {
-				errors: errors.array(),
-				sortidacurricular: sortidacurricular,
-			});
-		} else {
-			SortidaCurricular.create(req.body, function (error, newSortidaCurricular) {
-				const transporter = nodemailer.createTransport({
-					service: 'gmail',
-					auth: {
-					  user: 'USER', //email que enviara el correo de la sortida
-					  pass: 'PASSWORD' //hay que poner la contraseña del gmail que quiere enviar el correo
-					}
-				  });
+  static create_post(req, res) {
+    const errors = validationResult(req);
+    //console.log(errors.array());
+    // Tenim errors en les dades enviades
+    if (!errors.isEmpty()) {
+      var sortidacurricular = {
+        data_sortida: req.body.data_sortida,
+        email: req.body.email,
+        lloc: req.body.lloc,
+        ruta: req.body.ruta,
+        objectius: req.body.objectius,
+        grups: req.body.grups,
+        professors: req.body.professors,
+        hora_inici: req.body.hora_inici,
+        hora_arribada: req.body.hora_arribada,
+        estat: req.body.estat,
+        _id: req.params.id,
+      };
+      res.render("sortidescurriculars/new", {
+        errors: errors.array(),
+        sortidacurricular: sortidacurricular,
+      });
+    } else {
+      SortidaCurricular.create(req.body, function (error, newSortidaCurricular) {
+        const transporter = nodemailer.createTransport({
+          service: 'gmail',
+          auth: {
+            user: 'USER', //email que enviara el correo de la sortida
+            pass: 'PASSWORD' //hay que poner la contraseña del gmail que quiere enviar el correo
+          }
+          });
 
-				  const mailOptions = {
-					from: 'rolo836@vidalibarraquer.net',
-					to: req.body.email, // correo del destinatario obtenido del formulario
-					subject: 'Vidal i Barraquer Sortida Curricular',
-					text: 'Teniu una absència generada. Indiqueu per cada hora la tasca de lalumnat corresponenr'
-				  };
+          const mailOptions = {
+            from: "MAIL", // Correu desde on es envie el missatge.
+            to: req.body.email, // Correo del destinatari, obtingut desde el formulari.
+            subject: "Sortida curricular creada",
+            text: "Hola " + req.body.email + ", Teniu una absència generada. \n Indiqueu per cada hora la tasca de l'alumnat corresponent. \n Això es un correu generat automàticament, per favor no responeu aquest mail.",
+          };
 
-				  transporter.sendMail(mailOptions, function(error, info) {
-					if (error) {
-					  console.log(error);
-					} else {
-					  console.log('Correo electrónico enviado: ' + info.response);
-					}
-				  });
-				if (error) {
-					//console.log(error)
-					res.render("sortidescurriculars/new", { error: error.message });
-				} else {
-					res.redirect("/sortidescurriculars");
-				}
-			});
-		}
-	}
+          transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+              console.log(error);
+            } else {
+              console.log("Correu electrónic enviat: " + info.response);
+            }
+          });
+          if (error) {
+            res.render("sortidescurriculars/new", { error: error.message });
+          } else {
+            res.redirect("/sortidescurriculars");
+          }
+        }
+      );
+    }
+  }
 
+  static update_get(req, res, next) {
+    SortidaCurricular.findById(
+      req.params.id,
+      function (err, sortidacurricular) {
+        if (err) {
+          return next(err);
+        }
+        if (sortidacurricular == null) {
+          // No results.
+          var err = new Error("Sortida curricular no trobada");
+          err.status = 404;
+          return next(err);
+        }
+        // Success.
+        res.render("sortidescurriculars/update", {
+          sortidacurricular: sortidacurricular,
+        });
+      }
+    );
+  }
 
-
-	static update_get(req, res, next) {
-		SortidaCurricular.findById(
-			req.params.id,
-			function (err, sortidacurricular) {
-				if (err) {
-					return next(err);
-				}
-				if (sortidacurricular == null) {
-					// No results.
-					var err = new Error("Sortida curricular no trobada");
-					err.status = 404;
-					return next(err);
-				}
-				// Success.
-				res.render("sortidescurriculars/update", {
-					sortidacurricular: sortidacurricular,
-				});
-			}
-		);
-	}
-
-	static async update_post(req, res, next) {
+  static async update_post(req, res, next) {
     // Obtener la fecha actual
     const today = new Date();
     today.setHours(0, 0, 0, 0);
@@ -148,7 +147,19 @@ class sortidacurricularController {
         });
     }
 
-    // Si la fecha de salida es igual o posterior a la fecha actual, continuar con el proceso de actualización
+    // Obtener la fecha de salida del formulario update
+    const dataSortidaVacia = req.body.data_sortida;
+
+    // Comprobar si la data de sortida existeix
+    if (!dataSortidaVacia) {
+        // Si la fecha de salida es anterior a la fecha actual, mostrar un mensaje de error
+        return res.render("sortidescurriculars/update", {
+            error: "La data de sortida no pot estar buida",
+            sortidacurricular: req.body,
+        });
+    }
+
+    // Si la fecha de salida es igual o posterior a la fecha actual y no esta vacia, continuar con el proceso de actualización
     var sortidacurricular = new SortidaCurricular({
         data_sortida: req.body.data_sortida,
         email: req.body.email,
@@ -186,19 +197,19 @@ class sortidacurricularController {
 }
 
 
-	static async delete_get(req, res, next) {
-		res.render("sortidescurriculars/delete", { id: req.params.id });
-	}
+  static async delete_get(req, res, next) {
+    res.render("sortidescurriculars/delete", { id: req.params.id });
+  }
 
-	static async delete_post(req, res, next) {
-		SortidaCurricular.findByIdAndRemove(req.params.id, (error) => {
-			if (error) {
-				res.render("sortidescurriculars", { error: error.message });
-			} else {
-				res.redirect("/sortidescurriculars");
-			}
-		});
-	}
+  static async delete_post(req, res, next) {
+    SortidaCurricular.findByIdAndRemove(req.params.id, (error) => {
+      if (error) {
+        res.render("sortidescurriculars", { error: error.message });
+      } else {
+        res.redirect("/sortidescurriculars");
+      }
+    });
+  }
 }
 
 module.exports = sortidacurricularController;
