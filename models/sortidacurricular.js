0f286a02defee8bd428d2ee5553b7abebc3d7e72
josep <bfg9kappa@gmail.com>
2023-04-27 20:46:36 +0200

    populate abspre

--- models/sortidacurricular.js
+++ models/sortidacurricular.js
@@ -11,7 +11,7 @@ var SortidaCurricularSchema = new Schema({
 	ruta: { type: String },
 	objectius: { type: String },
 	grups: { type: String },
-	professor: { type: Schema.ObjectId, ref: "User", required: false },
+	professors: { type: Schema.ObjectId, ref: "User", required: false },
 	hora_inici: { type: String,
 		enum: ['08:00', '09:00', '10:00', '11:00', '11:30', '12:30', '13:30', '15:00', '16:00', '17:00', '18:30', '19:30', '20:30'],
     required: false },
