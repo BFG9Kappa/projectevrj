26257bed91e652270204dda08e2804f805cd4abe
josep <bfg9kappa@gmail.com>
2023-05-01 20:26:57 +0200

    update test

--- test/integration/baixesmediques.test.js
+++ test/integration/baixesmediques.test.js
@@ -5,9 +5,9 @@ const app = require("../../app.js");
 
 describe("Proves de integració per al controlador de baixes mediques", () => {
 	describe("GET /baixesmediques", () => {
-		it("Tindria que retornar codi de estat 200.", async () => {
+		it("Tindria que retornar codi de estat 302.", async () => {
 			const response = await request(app).get("/baixesmediques");
-			expect(response.status).to.equal(200);
+			expect(response.status).to.equal(302);
 		});
 	});
 });
