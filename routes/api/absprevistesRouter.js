9218c19dc7192384bab7dfd4f0b226107d7f387d
Robert <robertrlm16@gmail.com>
2023-05-05 10:39:17 +0200

    rutas protegidas

--- routes/api/absprevistesRouter.js
+++ routes/api/absprevistesRouter.js
@@ -2,10 +2,16 @@ var express = require("express");
 var router = express.Router();
 
 const absprevista_controller = require("../../controllers/api/absprevistesController");
+const checkAuth = require('../../middlewares/auth');
+const checkRoleAuth = require('../../middlewares/roleAuth');
 
-router.get("/all", absprevista_controller.all);
+router.get("/all", checkAuth, checkRoleAuth(['professor','administrador']), absprevista_controller.all);
+
+router.post("/create", checkAuth, checkRoleAuth(['professor','administrador']),absprevista_controller.rules, absprevista_controller.create);
+
+//router.get("/all", absprevista_controller.all);
 router.get("/", absprevista_controller.list);
-router.post("/create", absprevista_controller.rules, absprevista_controller.create);
+//router.post("/create", absprevista_controller.rules, absprevista_controller.create);
 router.delete("/delete/:id", absprevista_controller.delete);
 router.put("/update/:id", absprevista_controller.rules, absprevista_controller.update);
 
--- routes/api/baixesmediquesRouter.js
+++ routes/api/baixesmediquesRouter.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 const baixamedica_controller = require("../../controllers/api/baixesmediquesController");
 
-router.get("/all", baixamedica_controller.all);
+const checkAuth = require('../../middlewares/auth');
+const checkRoleAuth = require('../../middlewares/roleAuth');
+
+router.get("/all", checkAuth, checkRoleAuth(['professor','administrador']), baixamedica_controller.all);
+
+//router.get("/all", baixamedica_controller.all);
 router.get("/", baixamedica_controller.list);
 router.delete("/delete/:id", baixamedica_controller.delete);
 router.post("/create", baixamedica_controller.rules, baixamedica_controller.create);
